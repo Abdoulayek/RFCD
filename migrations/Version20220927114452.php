7b1108334232b5eca865fc5c689f92f8e7a64ee1
abdoulaye kante <abdoulayekante863@gmail.com>
2022-09-28 17:04:19 +0200

    fd

--- migrations/Version20220927114452.php
+++ migrations/Version20220927114452.php
@@ -29,17 +29,17 @@ final class Version20220927114452 extends AbstractMigration
     //   $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     //$this->addSql('ALTER TABLE certificateurs_user ADD CONSTRAINT FK_40ACFD1D2A2AA117 FOREIGN KEY (certificateurs_id) REFERENCES certificateurs (id) ON DELETE CASCADE');
     //$this->addSql('ALTER TABLE certificateurs_user ADD CONSTRAINT FK_40ACFD1DA76ED395 FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE');
-      $this->addSql('ALTER TABLE contact ADD CONSTRAINT FK_4C62E638265A0880 FOREIGN KEY (relation_certif_id) REFERENCES certificateurs (id)');
-    $this->addSql('ALTER TABLE stagiaires ADD CONSTRAINT FK_4A9FADC649793480 FOREIGN KEY (relationuser_id) REFERENCES user (id)');
+   $this->addSql('ALTER TABLE contact ADD CONSTRAINT FK_4C62E638265A0880 FOREIGN KEY (relation_certif_id) REFERENCES certificateurs (id)');
+ $this->addSql('ALTER TABLE stagiaires ADD CONSTRAINT FK_4A9FADC649793480 FOREIGN KEY (relationuser_id) REFERENCES user (id)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE certificateurs_user DROP FOREIGN KEY FK_40ACFD1D2A2AA117');
-        $this->addSql('ALTER TABLE contact DROP FOREIGN KEY FK_4C62E638265A0880');
+      //  $this->addSql('ALTER TABLE contact DROP FOREIGN KEY FK_4C62E638265A0880');
         $this->addSql('ALTER TABLE certificateurs_user DROP FOREIGN KEY FK_40ACFD1DA76ED395');
-        $this->addSql('ALTER TABLE stagiaires DROP FOREIGN KEY FK_4A9FADC649793480');
+       // $this->addSql('ALTER TABLE stagiaires DROP FOREIGN KEY FK_4A9FADC649793480');
         $this->addSql('DROP TABLE certificateurs');
         $this->addSql('DROP TABLE certificateurs_user');
         $this->addSql('DROP TABLE contact');
