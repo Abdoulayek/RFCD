5b731a37cd55f01885dc11c150b2e22121f8e0e1
abdoulaye kante <abdoulayekante863@gmail.com>
2022-09-29 12:14:25 +0200

    fd

--- migrations/Version20220929101109.php
+++ migrations/Version20220929101109.php
@@ -20,12 +20,12 @@ final class Version20220929101109 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE stagiaires ADD prof VARCHAR(255) NOT NULL');
+    //    $this->addSql('ALTER TABLE stagiaires ADD prof VARCHAR(255) NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE stagiaires DROP prof');
+    //    $this->addSql('ALTER TABLE stagiaires DROP prof');
     }
 }
