e6641463276244fda83d52981bdc6c7d40d60a18
abdoulaye kante <abdoulayekante863@gmail.com>
2022-09-29 12:26:15 +0200

    fd

--- migrations/Version20220929102145.php
+++ migrations/Version20220929102145.php
@@ -20,6 +20,7 @@ final class Version20220929102145 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE certificateurs (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) NOT NULL, prenom VARCHAR(255) NOT NULL, experiences VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE certificateurs DROP pr');
         $this->addSql('ALTER TABLE contact DROP p');
         $this->addSql('ALTER TABLE stagiaires DROP prf');
